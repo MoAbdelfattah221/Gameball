8568abf493195d5aeffc4a57651e80cd07568828
Mohamed-Abdelfattah <moabdelfattah221@gmail.com>
2022-10-06 23:38:11 +0200

    Gameball commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Menu Icon Function
+let menuIcon = document.querySelector(".menu-icon");
+let menu = document.querySelector(".menu");
+menuIcon.onclick = function(){
+    menu.classList.toggle("active")
+}
